4bca255d47f08492c8f0ed537c4d0b4c587b2047
martileos <martinevarez@gmail.com>
2016-04-12 10:59:06 -0600

    Archivo JS, funcion anonima y consola

---
+++ javascript/js/codigo.js
@@ -0,0 +1,10 @@
+var botonClic = function()
+{
+	alert("Clic del botón");
+}
+
+var nombre = "Martin";
+document.write("<strong>JS en acción</strong>"
+			  +" "+nombre);
+console.log("A punto de lanzar un mensaje");
+alert("Mensaje por pantalla");
