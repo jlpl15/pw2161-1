b4994636b670eb2ddce6ea5b8c774bb829d90ac1
martileos <martinevarez@gmail.com>
2016-05-11 10:59:46 -0600

    Validacion de usuario y clave con JS

--- jquery2/js/eventos.js
+++ jquery2/js/eventos.js
@@ -1,10 +1,13 @@
 var iniciaApp = function()
 {
 	var validarEntrada = function()
-	{
+	{	
+		//Invalida los eventos que 
+		//no corresponden a esta función.	
+		event.preventDefault();
 		var usuario = $("#txtUsuario").val();
 		var clave   = $("#txtClave").val();
-		//Validaciones
+		//******** Validaciones **********
 		//1.- Que no sean vacíos
 		if(usuario == "")
 		{
@@ -16,12 +19,29 @@ var iniciaApp = function()
 			alert("La clave no debe ser vacía");
 			$("#txtClave").focus();
 		}
-		//2.- 
+		//2.- Verificar usuario y contraseña
+		if(usuario=="pw" && clave=="1234")
+		{
+			// alert("Bienvenido "+usuario);
+			// Dar entrada al usuario
+			$("#datosUsuario").hide(); //escondemos
+			$("nav").show("slow"); //mostramos
+		}
+		else
+			alert("Usuario y/o contraseña incorrecta(s)");
 		console.log("Se disparó el submit");
 	}
 	$("#frmValidaEntrada").on("submit",validarEntrada);
 }
 $(document).on("ready",iniciaApp);
+
+
+
+
+
+
+
+
 
 
 
