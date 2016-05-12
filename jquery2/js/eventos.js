c8e856cc513a64680f2a3ee5fb9aeb14f9f3a2a7
martileos <martinevarez@gmail.com>
2016-05-12 11:02:06 -0600

    Iniciar AJAX para validar usuario. Archivo PHP vacio

--- jquery2/js/eventos.js
+++ jquery2/js/eventos.js
@@ -20,15 +20,26 @@ var iniciaApp = function()
 			$("#txtClave").focus();
 		}
 		//2.- Verificar usuario y contraseña
-		if(usuario=="pw" && clave=="1234")
-		{
-			// alert("Bienvenido "+usuario);
-			// Dar entrada al usuario
-			$("#datosUsuario").hide(); //escondemos
-			$("nav").show("slow"); //mostramos
-		}
-		else
-			alert("Usuario y/o contraseña incorrecta(s)");
+		var parametros="accion=validaEntrada"+
+					   "&usuario="+usuario+
+					   "&clave="+clave+
+					   "&id="+Math.random(); 
+		$.ajax({
+			beforeSend:function(){
+				console.log("Validar al usuario");
+			},
+			cache: false,
+			type: "POST",
+			dataType: "json",
+			url:"php/funciones.php",
+			data:parametros,
+			success: function(response){
+
+			},
+			error: function(xhr,ajaxOptions,thrownError){
+				console.log("Algo salió mal");
+			}
+		});
 		console.log("Se disparó el submit");
 	}
 	$("#frmValidaEntrada").on("submit",validarEntrada);
